45246417e985fdfd91e4f349b453db1dd1feaa10
Sarah <41387357+sarahselee223@users.noreply.github.com>
2021-02-12 21:15:47 -0800

    create swift basic files

---
+++ todo_list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todo_list
+//
+//  Created by Sarah Lee on 2/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
